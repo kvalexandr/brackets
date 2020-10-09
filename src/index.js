e5cdb62e709afb74a13d561a2ee00e34ac6c95d3
Alexandr <k.v.alexandr@gmail.com>
2020-10-09 22:12:24 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strLenBeforeChange = str.length;
+
+  for (let [bracketStart, bracketEnd] of bracketsConfig) {
+    str = str.split(`${bracketStart}${bracketEnd}`).join('');
+  }
+
+  if (str.length === 0) {
+    return true;
+  } else if (str.length === strLenBeforeChange) {
+    return false;
+  }
+
+  return check(str, bracketsConfig);
 }
